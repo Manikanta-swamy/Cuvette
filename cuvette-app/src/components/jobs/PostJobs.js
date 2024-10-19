71911e4083278b08f8e230db4336ac3fc686836e
manikanta swamy <20pa1a5706@vishnu.edu.in>
2024-10-19 23:48:24 +0530

    frontend changes

--- cuvette-app/src/components/jobs/PostJobs.js
+++ cuvette-app/src/components/jobs/PostJobs.js
@@ -154,15 +154,14 @@ const PostJobs = () => {
             required
             className="border p-2 w-4/12 rounded"
           />
-          
         </div>
         {/* Submit Button */}
         <button
-            type="submit"
-            className="bg-blue-600 mx-auto text-white px-4 py-2 rounded-md"
-          >
-            Post Job
-          </button>
+          type="submit"
+          className="bg-blue-600 mx-auto text-white px-4 py-2 rounded-md"
+        >
+          Post Job
+        </button>
       </form>
 
       {/* Toast Container */}
